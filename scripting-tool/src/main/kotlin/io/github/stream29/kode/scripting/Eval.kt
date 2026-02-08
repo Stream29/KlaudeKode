52631fb79de02766819e56245239dd1681d212e7
Stream <Stream_2@qq.com>
2026-02-08 17:44:02 +0900

    fix: kotlin scripting result handling

--- scripting-tool/src/main/kotlin/io/github/stream29/kode/scripting/Eval.kt
+++ scripting-tool/src/main/kotlin/io/github/stream29/kode/scripting/Eval.kt
@@ -1,14 +1,19 @@
 package io.github.stream29.kode.scripting
 
+import io.github.stream29.kode.scripting.EvalResult.*
 import kotlinx.coroutines.suspendCancellableCoroutine
 import kotlinx.serialization.Serializable
 import org.jetbrains.kotlin.mainKts.MainKtsScript
 import java.io.ByteArrayOutputStream
 import java.io.PrintStream
 import kotlin.coroutines.resume
+import kotlin.script.experimental.api.EvaluationResult
+import kotlin.script.experimental.api.ResultValue
 import kotlin.script.experimental.api.ResultWithDiagnostics
+import kotlin.script.experimental.api.asSuccess
 import kotlin.script.experimental.api.constructorArgs
 import kotlin.script.experimental.host.toScriptSource
+import kotlin.script.experimental.jvm.util.renderError
 
 @Serializable
 public sealed interface EvalResult {
@@ -27,7 +32,7 @@ public sealed interface EvalResult {
 
 public suspend fun eval(script: String): EvalResult = suspendCancellableCoroutine {
     val thread = Thread.startVirtualThread {
-        it.resume(evalInternal(script))
+        it.resumeWith(runCatching{ evalInternal(script) })
     }
     it.invokeOnCancellation { thread.interrupt() }
 }
@@ -49,12 +54,15 @@ internal fun evalInternal(script: String): EvalResult {
         val stdout = outputStream.toString(Charsets.UTF_8)
 
         return when (evaluationResult) {
-            is ResultWithDiagnostics.Success<*> -> EvalResult.Success(
-                returnValue = evaluationResult.value.toString(),
-                stdout = stdout
-            )
+            is ResultWithDiagnostics.Success<EvaluationResult> ->
+                when(val returnValue = evaluationResult.value.returnValue) {
+                    is ResultValue.Value -> Success(returnValue.value.toString(), stdout)
+                    is ResultValue.Error -> Failure(returnValue.renderError(), stdout)
+                    is ResultValue.Unit -> Success("kotlin.Unit", stdout)
+                    is ResultValue.NotEvaluated -> Failure("Script did not evaluate", stdout)
+                }
 
-            is ResultWithDiagnostics.Failure -> EvalResult.Failure(
+            is ResultWithDiagnostics.Failure -> Failure(
                 message = evaluationResult.reports.joinToString("\n"),
                 stdout = stdout
             )
